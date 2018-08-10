28a80c3dac558df865764d83e9c6db670894b4d0
Tommy Hodgins <tomhodgins@gmail.com>
2018-08-10 10:15:53 -0400

    consolidate plugin object

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 const qaffeine = require('qaffeine')
 const {element} = require('./src/element-query-plugin.js')
 
-// Plugins
-const plugins = {
-  stylesheet: {
-    element
-  },
-  rule: {
-
-  }
-}
-
 qaffeine(
-  plugins,
+  {
+    stylesheet: {
+      element
+    },
+    rule: {}
+  },
   'src/stylesheet.css',
   'dist/styles.js',
   'dist/styles.css'
