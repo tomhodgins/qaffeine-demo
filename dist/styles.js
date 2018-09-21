df486f55f7d245d7b7c4fc75b6c71af4eb843f6a
Tommy Hodgins <tomhodgins@gmail.com>
2018-09-21 19:22:01 -0400

    Annotate element query plugins

--- dist/styles.js
+++ dist/styles.js
@@ -62,6 +62,7 @@ const customAtRule = {}
 
 customAtRule.element = function (selector='', ...extra) {
 
+  // Built-in element query tests
   const eq = {
     minWidth: (el, number) => number <= el.offsetWidth,
     maxWidth: (el, number) => number >= el.offsetWidth,
@@ -100,60 +101,72 @@ customAtRule.element = function (selector='', ...extra) {
 
   switch (extra.length) {
 
+    // If 2 arguments present, use as options and custom plugin
     case 3:
       options = extra[0]
       plugins = Object.assign(extra[1], plugins)
       stylesheet = extra[2]
     break
 
+    // If 1 argument present, use as options
     case 2:
       options = extra[0]
       stylesheet = extra[1]
     break
 
+    // If no arguments present, run as scoped style
     case 1:
       stylesheet = extra[0]
     break
 
   }
 
+  // For each tag in the document matching the CSS selector
   return Array.from(document.querySelectorAll(selector))
 
+    // Process each tag and return a string of CSS that tag needs
     .reduce((styles, tag, count) => {
 
+      // Create a custom data atribute we can assign if the tag matches
       const attr = (selector + Object.entries(options)).replace(/\W/g, '')
 
-      if (Object.keys(options).every(test => {
+      // Test tag to see if it passes all conditions
+      if (
 
-        if (tag[test] !== null && typeof options[test] === 'function') {
+        // Pass if every condition given is true
+        Object.keys(options).every(test => {
 
-          return options[test](tag[test])
+          // If condition is a property of the tag, test the property
+          if (
+            tag[test] !== null 
+            && typeof options[test] === 'function'
+          ) return options[test](tag[test])
 
-        } else if (tag[test]) {
+          // Otherwise if condition is a method of the tag, run the method
+          else if (tag[test]) return options[test] === tag[test]
 
-          return options[test] === tag[test]
+          // Otherwise if condition matches a loaded plugin, run the plugin
+          else if (plugins[test]) return plugins[test](tag, options[test])
 
-        } else if (plugins[test]) {
+        })
 
-          return plugins[test](tag, options[test])
+      ) {
 
-        } else {
-
-          return false
-
-        }
-
-      })) {
-
+        // If the tag passes, set a custom data attribute
         tag.setAttribute(`data-element-atRule-${attr}`, count)
+
+        // Add CSS stylesheet to output, replacing [--self] with the current tag
         styles += stylesheet.replace(
           /:self|\$this|\[--self\]/g,
           `${selector}[data-element-atRule-${attr}="${count}"]`
         )
+
+        // Increase our count of matching tags by 1
         count++
 
       } else {
 
+        // Otherwise if tag fails tests, remove custom data attribute value
         tag.setAttribute(`data-element-atRule-${attr}`, '')
 
       }
@@ -168,6 +181,7 @@ const customStyleRule = {}
 
 customStyleRule.element = function (selector='', ...extra) {
 
+  // Built-in element query tests
   const eq = {
     minWidth: (el, number) => number <= el.offsetWidth,
     maxWidth: (el, number) => number >= el.offsetWidth,
@@ -206,57 +220,69 @@ customStyleRule.element = function (selector='', ...extra) {
 
   switch (extra.length) {
 
+    // If 2 arguments present, use as options and custom plugin
     case 3:
       options = extra[0]
       plugins = Object.assign(extra[1], plugins)
       rule = extra[2]
     break
 
+    // If 1 argument present, use as options
     case 2:
       options = extra[0]
       rule = extra[1]
     break
 
+    // If no arguments present, run as scoped style
     case 1:
       rule = extra[0]
     break
 
   }
 
+  // For each tag in the document matching the CSS selector
   return Array.from(document.querySelectorAll(selector))
 
+    // Process each tag and return a string of CSS that tag needs
     .reduce((styles, tag, count) => {
 
+      // Create a custom data atribute we can assign if the tag matches
       const attr = (selector + Object.entries(options)).replace(/\W/g, '')
 
-      if (Object.keys(options).every(test => {
+      // Test tag to see if it passes all conditions
+      if (
 
-        if (tag[test] !== null && typeof options[test] === 'function') {
+        // Pass if every condition given is true
+        Object.keys(options).every(test => {
 
-          return options[test](tag[test])
+          // If condition is a property of the tag, test the property
+          if (
+            tag[test] !== null 
+            && typeof options[test] === 'function'
+          ) return options[test](tag[test])
 
-        } else if (tag[test]) {
+          // Otherwise if condition is a method of the tag, run the method
+          else if (tag[test]) return options[test] === tag[test]
 
-          return options[test] === tag[test]
+          // Otherwise if condition matches a loaded plugin, run the plugin
+          else if (plugins[test]) return plugins[test](tag, options[test])
 
-        } else if (plugins[test]) {
+        })
 
-          return plugins[test](tag, options[test])
+      ) {
 
-        } else {
-
-          return false
-
-        }
-
-      })) {
-
+        // If the tag passes, set a custom data attribute
         tag.setAttribute(`data-element-selector-${attr}`, count)
+
+        // Add CSS rule to output, adding selector for the current tag
         styles += `${selector}[data-element-selector-${attr}="${count}"] { ${rule} }\n`
+
+        // Increase our count of matching tags by 1
         count++
 
       } else {
 
+        // Otherwise if tag fails tests, remove custom data attribute value
         tag.setAttribute(`data-element-selector-${attr}`, '')
 
       }
--- src/element-query-selector.js
+++ src/element-query-selector.js
@@ -1,5 +1,6 @@
 module.exports = function(selector='', ...extra) {
 
+  // Built-in element query tests
   const eq = {
     minWidth: (el, number) => number <= el.offsetWidth,
     maxWidth: (el, number) => number >= el.offsetWidth,
@@ -38,57 +39,69 @@ module.exports = function(selector='', ...extra) {
 
   switch (extra.length) {
 
+    // If 2 arguments present, use as options and custom plugin
     case 3:
       options = extra[0]
       plugins = Object.assign(extra[1], plugins)
       rule = extra[2]
     break
 
+    // If 1 argument present, use as options
     case 2:
       options = extra[0]
       rule = extra[1]
     break
 
+    // If no arguments present, run as scoped style
     case 1:
       rule = extra[0]
     break
 
   }
 
+  // For each tag in the document matching the CSS selector
   return Array.from(document.querySelectorAll(selector))
 
+    // Process each tag and return a string of CSS that tag needs
     .reduce((styles, tag, count) => {
 
+      // Create a custom data atribute we can assign if the tag matches
       const attr = (selector + Object.entries(options)).replace(/\W/g, '')
 
-      if (Object.keys(options).every(test => {
+      // Test tag to see if it passes all conditions
+      if (
 
-        if (tag[test] !== null && typeof options[test] === 'function') {
+        // Pass if every condition given is true
+        Object.keys(options).every(test => {
 
-          return options[test](tag[test])
+          // If condition is a property of the tag, test the property
+          if (
+            tag[test] !== null 
+            && typeof options[test] === 'function'
+          ) return options[test](tag[test])
 
-        } else if (tag[test]) {
+          // Otherwise if condition is a method of the tag, run the method
+          else if (tag[test]) return options[test] === tag[test]
 
-          return options[test] === tag[test]
+          // Otherwise if condition matches a loaded plugin, run the plugin
+          else if (plugins[test]) return plugins[test](tag, options[test])
 
-        } else if (plugins[test]) {
+        })
 
-          return plugins[test](tag, options[test])
+      ) {
 
-        } else {
-
-          return false
-
-        }
-
-      })) {
-
+        // If the tag passes, set a custom data attribute
         tag.setAttribute(`data-element-selector-${attr}`, count)
+
+        // Add CSS rule to output, adding selector for the current tag
         styles += `${selector}[data-element-selector-${attr}="${count}"] { ${rule} }\n`
+
+        // Increase our count of matching tags by 1
         count++
 
       } else {
 
+        // Otherwise if tag fails tests, remove custom data attribute value
         tag.setAttribute(`data-element-selector-${attr}`, '')
 
       }
